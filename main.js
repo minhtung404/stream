28178cf154817a12a46172894f0126af6f594aed
minhtung404 <30457418+minhtung404@users.noreply.github.com>
2017-07-27 07:52:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -62,7 +62,27 @@ var peer = new Peer({
   secure:true,
    port:443,
    debug:3,
-  config:configHostTurn});
+  config:{ 'iceServers': [{ url: 'stun:stun01.sipphone.com' },
+ { url: 'stun:stun.ekiga.net' },
+ { url: 'stun:stun.fwdnet.net' },
+ { url: 'stun:stun.ideasip.com' },
+ { url: 'stun:stun.iptel.org' },
+ { url: 'stun:stun.rixtelecom.se' },
+ { url: 'stun:stun.schlund.de' },
+ { url: 'stun:stun.l.google.com:19302' },
+ { url: 'stun:stun1.l.google.com:19302' },
+ { url: 'stun:stun2.l.google.com:19302' },
+ { url: 'stun:stun3.l.google.com:19302' },
+ { url: 'stun:stun4.l.google.com:19302' },
+ { url: 'stun:stunserver.org' },
+ { url: 'stun:stun.softjoys.com' },
+ { url: 'stun:stun.voiparound.com' },
+ { url: 'stun:stun.voipbuster.com' },
+ { url: 'stun:stun.voipstunt.com' },
+ { url: 'stun:stun.voxgratia.org' },
+ { url: 'stun:stun.xten.com'},
+    ]}
+});
 peer.on('open',function(data){
   $('#myPeer').html(data);
 });
@@ -91,10 +111,10 @@ peer.on("call",call=>{
           call.answer(stream);
           // Tong dai khong can stream chinh minh
           //playStream('localStream',stream)
-          console.log(stream);
+        //  console.log(stream);
           call.on('stream',dataStream=>{
             playStream('traLoi',dataStream)
-            console.log("dataSteam"+dataStream);
+          //  console.log("dataSteam"+dataStream);
           });
 
         });
